a064b1ab347f14d6aee9cc953bb0642d1998289c
Rahul Keshari <rahulkeshri2244@gmail.com>
2025-08-03 15:32:15 +0000

    Rahul

---
+++ 01_basic/03_conversion.js
@@ -0,0 +1,21 @@
+let score = "33"
+console.log(typeof score);//string hoga
+console.log(typeof(score)); //string hoga
+
+let valueInNumber = Number(score)
+console.log(typeof score);
+console.log(typeof valueInNumber);
+
+// "33"=> 33 convert ho jyaga
+//"33abc"=> NaN
+//true => 1 , false => 0 convert ho jyaga
+let isLoggedIn = 1
+ let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+let somenumber = 33
+let stringnumber = String(somenumber)
+console.log(stringnumber);
+console.log(typeof stringnumber); // output => string me convert ho gaya hai
+
+

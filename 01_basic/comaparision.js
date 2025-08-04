13617f25c66e5436c43b9ac5c89206195a15d1c0
Rahul Keshari <rahulkeshri2244@gmail.com>
2025-08-04 01:44:05 +0000

    Rahul

---
+++ 01_basic/comaparision.js
@@ -0,0 +1,21 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log(null>0);  //false
+// console.log(null==0); //false
+// console.log(null>=0); //true
+
+
+// console.log(undefined=0); // 0
+// console.log(undefined>0); //false
+// console.log(undefined<0); //false
+
+// === it checks data types also , wether the data types are same or not
+
+console.log("2"===2); // false
+
+
+

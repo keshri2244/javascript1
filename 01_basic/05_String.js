66caa0751f84c0efb39e3f2f16a7f67476326ebb
Rahul Keshari <rahulkeshri2244@gmail.com>
2025-08-06 02:29:24 +0000

    String all concepts

---
+++ 01_basic/05_String.js
@@ -0,0 +1,39 @@
+// const name = "Rahul"
+// const repoCount = 50
+
+// console.log(name + repoCount + "Value");
+
+// console.log('Hello my name is ${name} and my repo count is ${repoCount}')
+
+const gameName = new String('Rahul is a CAT exam topper ') // object method of string
+// console.log(gameName[0]);
+// console.log(gameName[1]);
+// console.log(gameName[2]);
+// console.log(gameName[3]);
+// console.log(gameName[4]);
+// console.log(gameName.__proto__); // object{}
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(4));
+
+// console.log(gameName.indexOf('l')); //4
+
+// const newString = gameName.substring(0,2) // ra
+// console.log(newString);
+
+// const anotherstring = gameName.slice(-3,1)  // can take negative values
+// console.log(anotherstring); //
+
+// const newString1 = "        DHONI          "
+// console.log(newString1);
+// console.log(newString1.trim()); // remove all the spaces and prints the trim version of string
+
+// const url = "https://rahul.com/rahul%2244keshari"
+// console.log(url.replace('%2244','_'));
+// console.log(url.includes('rahul')); // used to search
+ 
+console.log(gameName.split('_'));
+
+
+
--- 01_basic/dataTypesSummary.js
+++ 01_basic/dataTypesSummary.js
@@ -25,10 +25,10 @@ const bigNumber = 3697854215879n // big int ban gaya hai
 
 
 
-// 2) Non-primitive Datatypes
-//        Arrays  =>  object
-//        Function  =>  function
-//        Object  =>  object
+2) Non-primitive Datatypes
+       Arrays  =>  object
+       Function  =>  function
+       Object  =>  object
 
 
 const heros = ["Shaktiman","naagraj","doga"];
@@ -40,4 +40,44 @@ let myobj = {
 const myFunction = function(){
     console.log("Hello World\n");
     
+// }
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+/*
+=>stack ( used in primitive data types)
+jo bhi variable decleare kiya haiuska copy milta hai
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+=>primitive (used in non primitive data types)
+yaha bhi jo bhi object heap k andar chala jata hai uska refrence milta hai , jo bhi chages datatypes pe hoga vo original values pe hoga
+
+
+*/
+
+let myYoutubename = "RahulKeshari.com"
+let anotherName = myYoutubename
+anotherName = "chaiaurcode"
+console.log(anotherName);
+console.log(myYoutubename);
+
+ let userOne = {
+    email : "rahulkeshri2244@gmail.com" ,
+    upi :  "user@ybl"
 }
+
+let userTwo = userOne
+
+userTwo.email = "rahul@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+
+

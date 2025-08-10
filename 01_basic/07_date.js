58158bad5fc3024bc3b968c9c3e25f51b1420db8
Rahul Keshari <rahulkeshri2244@gmail.com>
2025-08-10 05:56:14 +0000

    rahul

---
+++ 01_basic/07_date.js
@@ -0,0 +1,22 @@
+// Dates 
+
+let myDate = new Date()
+// console.log(myDate.toString()); //Fri Aug 08 2025 08:02:46 GMT+0000 (Coordinated Universal Time)
+// console.log(myDate.toDateString()); //Fri Aug 08 2025
+// console.log(myDate.toLocaleString()); //8/8/2025, 8:02:46 AM
+// console.log(typeof myDate); //object
+
+// let myCreatedDate = new Date(2025,0,23)
+// console.log(myCreatedDate.toDateString());  //Thu Jan 23 2025
+
+// let myCreatedDate = new Date(2025,0,23 , 5, 3)
+// console.log(myCreatedDate.toLocaleString()); //1/23/2025, 5:03:00 AM
+
+// let myCreatedDate = new Date("2025-05-31")
+// console.log(myCreatedDate.toLocaleString()); //5/31/2025, 12:00:00 AM
+
+let myTimeStamp =  Date.now()
+console.log( myTimeStamp);
+
+
+

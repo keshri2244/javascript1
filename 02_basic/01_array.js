528f4b535d5de8ca781ed203a7a084255d351091
Rahul Keshari <rahulkeshri2244@gmail.com>
2025-08-10 07:45:34 +0000

    Array

---
+++ 02_basic/01_array.js
@@ -0,0 +1,48 @@
+const myArr = [0,1,2,3,4,5]
+// const myHeors = [ "Rahul", "Ved"]
+// const myArr2 = new Array(1,2,3,4,)
+// console.log(myArr[0]);
+// console.log(myHeors[1]);
+// console.log(myArr2[1]);
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.push(8)
+// myArr.push(9)
+// myArr.push(10)
+// myArr.push(11)
+// myArr.push(12)
+// myArr.push(13)
+// myArr.push(14)
+// myArr.push(15)
+// myArr.push(16)
+// console.log(myArr);
+// myArr.pop()
+// console.log(myArr);
+// myArr.unshift(9)  // koi bhi element ko first me shift karne k liye
+// myArr.shift()
+// console.log(myArr);
+
+
+
+// console.log(myArr.includes(9)); //teels wether that element is present in array or not
+// console.log(myArr.indexOf(9)); //-1
+// console.log(myArr.indexOf(3));
+
+
+
+// const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log(newArr);
+
+ const myn1 = myArr.slice(1,3)
+ console.log(myn1);
+
+ console.log("B ", myArr);
+
+ const myn2 = myArr.splice(1,3)
+ console.log(myn1);
+ console.log(myn2);
+ 
+ 
